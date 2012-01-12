a3dfb07a2fd48f7e141a357d66e8bb7776f4786e
Elia Schito <perlelia@gmail.com>
2012-01-12 11:28:51 +0100

    Better style

--- basecamp_todo_id.user.js
+++ basecamp_todo_id.user.js
@@ -1,27 +1,39 @@
 // ==UserScript==
-// @name           Basecamp Todo ID
 // @namespace      de.knuspermagier
+// @name           Basecamp Todo ID
 // @description    Shows the todo id in basecamp
 // @include        https://*.basecamphq.com/*
-// @version        1.1
+// @version        1.2
 // ==/UserScript==
 
-var items = document.getElementsByTagName("div");
+(function () {
+  var items, i, item, link, itemId, span, style, copyLink, label;
+  
+  items = document.getElementsByTagName("div");
 
-for(var i = 0; i < items.length; i++) {
-	var item = items[i];
-	
-	if(item.getAttribute('class') != 'widget list_widget item_wrapper commentable') {
-		continue;
-	}
-	
-	var itemId = item.getAttribute('record');
-	
-	if(itemId == null) {
-		continue;
-	}
-	
-	var span = document.getElementById('item_wrap_' + itemId);
-	
-	span.innerHTML = '[' + itemId + '] ' + span.innerHTML;
-}
+  for(i = 0; i < items.length; i++) {
+    item = items[i];
+  
+    if(item.getAttribute('class') != 'widget list_widget item_wrapper commentable') {
+      continue;
+    }
+  
+    itemId = item.getAttribute('record');
+  
+    if(itemId == null) {
+      continue;
+    }
+  
+    span  = document.getElementById('list_17801514_item_' + itemId + '_text');
+    style = 'background:#aaa;color:white;border-radius:1em;text-decoration:none;padding:0.2em 1em;font-size:0.8em;margin:0 0.3em;';
+    url   = window.location.href.replace(/(projects\/\d+[^\/]+\/).*$/, '$1/todo_items/'+itemId+'/comments');
+    id    = 'basecamp-todo-id-extension-'+itemId;
+    func  = 'document.getElementById(\"'+id+'\").select();document.execCommand(\"Copy\");'
+    
+    link  = '<a href="'+url+'" style="'+style+'" id="'+id+'">#'+itemId+'</a>';
+    label  = '<span style="'+style+'" id="'+id+'">#'+itemId+'</a>';
+    copyLink = '<a style="'+style+'" onclick="'+func+'">&rarr; clipboard</a>'
+    span.innerHTML = span.innerHTML+label;
+  }
+
+})();
